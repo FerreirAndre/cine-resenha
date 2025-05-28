b3ca44e0f552e857fa5eae25afbb3f32f6d3660b
FerreirAndre <adrecruz15@gmail.com>
2025-05-28 00:32:18 -0300

    finishing repositories implementation

--- Cine.Resenha.Persistence/PersistenceServiceRegistration.cs
+++ Cine.Resenha.Persistence/PersistenceServiceRegistration.cs
@@ -1,13 +1,23 @@
-﻿using Cine.Resenha.Persistence.DatabaseContext;
+﻿using Cine.Resenha.Application.Contracts.Persistence;
+using Cine.Resenha.Persistence.DatabaseContext;
+using Cine.Resenha.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cine.Resenha.Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        services.AddDbContext<MovieDatabaseContext>();
+        services.AddDbContext<MovieDatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddScoped(typeof(IMovieRepository), typeof(MovieRepository));
         return services;
     }
 }
---
+++ Cine.Resenha.Persistence/Repositories/MovieRepository.cs
@@ -0,0 +1,46 @@
+using Cine.Resenha.Application.Contracts.Persistence;
+using Cine.Resenha.Domain;
+using Cine.Resenha.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cine.Resenha.Persistence.Repositories;
+
+public class MovieRepository(MovieDatabaseContext context) : IMovieRepository
+{
+    private readonly MovieDatabaseContext _context = context;
+
+    public async Task<IReadOnlyList<Movie>> GetAsync()
+    {
+         return await _context.Set<Movie>().AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Movie> GetByIdAsync(int id)
+    {
+        return await _context.Set<Movie>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x=>x.Id == id);
+    }
+
+    public async Task CreateAsync(Movie movie)
+    {
+        await _context.AddAsync(movie);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Movie movie)
+    {
+        _context.Update(movie);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Movie movie)
+    {
+        _context.Remove(movie);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> MovieTitleUnique(string title)
+    {
+        return await _context.Set<Movie>().AnyAsync(x => x.Title == title) == false;
+    }
+}
